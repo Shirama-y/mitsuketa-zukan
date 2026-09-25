decc2efb7e1c59754039ad6102d9d6c72168fde2
suganotatsuki-max <suganotatsuki@gmail.com>
2026-09-25 13:41:45 +0900

    Cache visual assets for offline app use

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
-const CACHE_NAME = "mitsuketa-zukan-night-library-v1";
-const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
+const CACHE_NAME = "mitsuketa-zukan-system-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./app-icon.svg",
+  "./assets/library-backdrop.svg",
+  "./assets/shelf-wood.svg",
+  "./assets/cover-leather.svg",
+  "./assets/cover-linen.svg",
+  "./assets/cover-grain.svg",
+  "./assets/empty-collection.svg"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
